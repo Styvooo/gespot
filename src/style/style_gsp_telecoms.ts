1853f371f1c46849742eac5bf1dbe5fa58a62d92
FLacombe <francois.lacombe@infos-reseaux.com>
2026-05-17 19:28:16 +0200

    Fix

--- src/style/style_gsp_telecoms.ts
+++ src/style/style_gsp_telecoms.ts
@@ -78,23 +78,23 @@ export default function layers(): LayerSpecificationWithZIndex[] {
     }
   },
   {
-        zorder: 267,
-        id: 'telecoms_pole_transition',
-        type: 'symbol',
-        filter: all(utilityTelecom_p, ['==', ['get', 'type'], 'pole'], get('transition')),
-        source: 'gespot',
-        'source-layer': 'utility_support',
-        minzoom: 14.5,
-        layout: {
-          'icon-image': 'power_pole_transition',
-          'icon-offset': literal([-20, 0]),
-          'icon-size': interpolate(zoom, [
-            [14, 0.3],
-            [20, 1]
-          ]),
-          'icon-allow-overlap': true
-        }
-      },
+    zorder: 267,
+    id: 'telecoms_pole_transition',
+    type: 'symbol',
+    filter: all(utilityTelecom_p, ['==', ['get', 'type'], 'pole'], get('transition')),
+    source: 'gespot',
+    'source-layer': 'utility_support',
+    minzoom: 14.5,
+    layout: {
+      'icon-image': 'power_pole_transition',
+      'icon-offset': literal([-20, 0]),
+      'icon-size': interpolate(zoom, [
+        [14, 0.3],
+        [20, 1]
+      ]),
+      'icon-allow-overlap': true
+    }
+  },
   {
     zorder: 310,
     id: 'telecoms_pole_symbol',
